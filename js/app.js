d9bf6b19dee4c0e4e3afeb7b60f77da07aeac1ad
raselalamjubayed <raselalam838@gmil.com>
2021-09-18 00:38:15 +0600

    city hot tem added

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const API_KEY = `4eda8594a1fc06928bebd92c3d30b119`
+const searchTempautre = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main)
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon')
+    imgIcon.setAttribute('src', url);
+}
